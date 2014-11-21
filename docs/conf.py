f7d098377c7dc67d8e0e6d49325b6daa1a6c79f2
Christian Schramm <csch@solute.de>
2014-11-21 13:15:09 +0100

    Try to fix compilation errors on readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -263,3 +263,21 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+# Fix readthedocs.org compilation fails
+import sys
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import Mock as MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = ['pyicu', 'python-Levenshtein', 'numpy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
